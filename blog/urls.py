4950a2fafeeabfe470e7864d7b9b3b79e49cebf5
Your Name <you@example.com>
2021-08-13 13:53:47 +0000

    micro modify

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', views.PostList.as_view()),
-    # path('', views.index),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('prediction/', views.upload_image, name = 'upload_image'),
-    path('prediction1/', views.prediction, name = 'prediction'),
-    path('list/', views.image_list, name = 'image_list'),
+    path('prediction1/', views.prediction, name = 'prediction')
 ]
